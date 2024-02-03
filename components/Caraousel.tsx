a488c3975b07710e4a0ccd4345e06c784eefa9ef
darshanbajania <darshanbajania1999@gmail.com>
2024-02-03 16:45:44 +0530

    fix:  home page carousal

--- components/Caraousel.tsx
+++ components/Caraousel.tsx
@@ -1,61 +1,28 @@
 import React from "react";
-import Slider from "react-slick";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
+import Flickity from "react-flickity-component";
+import "flickity/css/flickity.css";
 const Caraousel = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    adaptiveHeight: true, // Adjusts the height of the carousel based on the current slide
-    prevArrow: (
-      <button className="carousel-prev absolute top-1/2 left-4 transform -translate-y-1/2">
-        {"<"}
-      </button>
-    ),
-    nextArrow: (
-      <button className="carousel-next absolute top-1/2 right-4 transform -translate-y-1/2">
-        {">"}
-      </button>
-    ),
-  };
   return (
-    <Slider {...settings} className="relative overflow-hidden ">
-      {/* Carousel Items */}
-      <div className="carousel-item h-64 md:h-96 lg:h-128 ">
-        {/* Your content for the first item */}
+    <Flickity>
+      <div style={{ marginLeft: 5, marginRight: 5, width: "100%" }}>
         <img
-          // src="https://via.placeholder.com/800x400"
+          style={{ width: "100%" }}
           src="https://t4.ftcdn.net/jpg/04/45/14/67/360_F_445146770_BBDoeRCg2l0gz09D7D63NqgUYmh1bE34.jpg"
-          alt="Carousel Item 1"
-          className="w-full h-full object-contain"
         />
       </div>
-
-      <div className="carousel-item h-64 md:h-96 lg:h-128 ">
-        {/* Your content for the second item */}
+      <div style={{ marginLeft: 5, marginRight: 5, width: "100%" }}>
         <img
-          src="https://via.placeholder.com/800x400"
-          alt="Carousel Item 2"
-          className="w-full h-full object-contain "
+          style={{ width: "100%" }}
+          src="https://t4.ftcdn.net/jpg/04/45/14/67/360_F_445146770_BBDoeRCg2l0gz09D7D63NqgUYmh1bE34.jpg"
         />
       </div>
-
-      <div className="carousel-item h-64 md:h-96 lg:h-128">
-        {/* Your content for the third item */}
+      <div style={{ marginLeft: 5, marginRight: 5, width: "100%" }}>
         <img
-          src="https://via.placeholder.com/800x400"
-          alt="Carousel Item 3"
-          className="w-full h-full object-contain "
+          style={{ width: "100%" }}
+          src="https://t4.ftcdn.net/jpg/04/45/14/67/360_F_445146770_BBDoeRCg2l0gz09D7D63NqgUYmh1bE34.jpg"
         />
       </div>
-
-      {/* Add more items as needed */}
-    </Slider>
+    </Flickity>
   );
 };
 
