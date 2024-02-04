482efae2133e08cd47bb98cfb4dd7ae291abb7d1
darshanbajania <darshanbajania1999@gmail.com>
2024-02-04 19:59:44 +0530

    refactor: create component for product carousal

--- app/viewProduct/[id]/page.tsx
+++ app/viewProduct/[id]/page.tsx
@@ -1,8 +1,8 @@
 // import { Image } from "@nextui-org/react";
 import React from "react";
-import Image from "next/image";
 import { GET_PRODUCT_DETAILS } from "@/lib/queries";
 import { getClient } from "@/lib/client";
+import ProductCarousal from "@/components/productCarousal";
 
 const page = async ({ params }) => {
   const client = getClient();
@@ -16,60 +16,20 @@ const page = async ({ params }) => {
     <div>
       <div className="mx-auto p-4  flex flex-wrap">
         {/* Column 1 */}
-        <div className="w-full md:w-1/2 p-4">
-          <div className="bg-white p-4 shadow-md rounded-md mb-4 md:mb-0 ">
-            <div className="flex  justify-center">
-              <img
-                className="w-full md:w-2/3  "
-                // src="https://www.theindiantoys.com/wp-content/uploads/woocommerce-placeholder.png"
-                src={data?.products_product_by_pk?.productImages?.[0]?.image}
-                width={"80%"}
-                style={{ alignSelf: "center" }}
-              />
-            </div>
-            <div className="" style={{ width: "100%" }}>
-              <div
-                className="flex  overflow-x-scroll align-middle"
-                style={{
-                  width: "100%",
-                }}
-              >
-                {data?.products_product_by_pk?.productImages?.map((item) => {
-                  return (
-                    <div key={item.id} className="mr-5 mt-10">
-                      <div
-                        className="flex justify-center bg-gray-100 rounded-md"
-                        style={{
-                          height: 100,
-                          width: 100,
-
-                          padding: 10,
-                        }}
-                      >
-                        <img
-                          src={item?.image}
-                          style={{
-                            alignSelf: "center",
-                            height: "100%",
-                            width: "100%",
-                            objectFit: "contain",
-                          }}
-                        />
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-        </div>
+        <ProductCarousal
+          productData={data?.products_product_by_pk?.productImages}
+        />
 
         {/* Column 2 */}
         <div className="w-full md:w-1/2 p-4">
           <div className="bg-white p-4 shadow-md rounded-md">
-            <h1>{data?.products_product_by_pk?.label}</h1>
-            <p>{data?.products_product_by_pk?.description}</p>
-            <p>₹{data?.products_product_by_pk?.price}</p>
+            <h1 className="text-3xl">{data?.products_product_by_pk?.label}</h1>
+            <p className="text-base text-current">
+              {data?.products_product_by_pk?.description}
+            </p>
+            <p className="text-2xl mt-8">
+              ₹{data?.products_product_by_pk?.price}
+            </p>
           </div>
         </div>
       </div>
---
+++ components/productCarousal.tsx
@@ -0,0 +1,64 @@
+"use client";
+import React, { useState } from "react";
+
+const ProductCarousal = ({ productData }) => {
+  const [selectedProduct, setSelectedProduct] = useState(productData[0]);
+
+  return (
+    <div className="w-full md:w-1/2 p-4">
+      <div className="bg-white p-4 shadow-md rounded-md mb-4 md:mb-0 ">
+        <div className="flex  justify-center">
+          <div style={{ height: 350 }} className=" bg-gray-100 ">
+            <img
+              // src="https://www.theindiantoys.com/wp-content/uploads/woocommerce-placeholder.png"
+              src={selectedProduct?.image}
+              style={{ height: "100%", width: "100%", objectFit: "contain" }}
+            />
+          </div>
+        </div>
+        <div className="" style={{ width: "100%" }}>
+          <div
+            className="flex  overflow-x-scroll align-middle"
+            style={{
+              width: "100%",
+            }}
+          >
+            {productData?.map((item) => {
+              return (
+                <div
+                  onClick={() => {
+                    setSelectedProduct(item);
+                  }}
+                  key={item.id}
+                  className="mr-5 mt-10"
+                >
+                  <div
+                    className="flex justify-center bg-gray-100 rounded-md"
+                    style={{
+                      height: 100,
+                      width: 100,
+
+                      padding: 10,
+                    }}
+                  >
+                    <img
+                      src={item?.image}
+                      style={{
+                        alignSelf: "center",
+                        height: "100%",
+                        width: "100%",
+                        objectFit: "contain",
+                      }}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCarousal;
