22725b925de7c305e657a2af0bf6107a1a4ed260
darshanbajania <darshanbajania1999@gmail.com>
2024-02-10 19:40:17 +0530

    feat: Navbar avatar setup

--- components/AuthSection.tsx
+++ components/AuthSection.tsx
@@ -1,25 +1,65 @@
-import { Button, Link, NavbarContent, NavbarItem } from "@nextui-org/react";
-import React from "react";
+import {
+  Avatar,
+  Button,
+  Link,
+  NavbarContent,
+  NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
 import AuthProviders from "./AuthProviders";
-import { signOut, useSession } from "next-auth/react";
+import { getSession, signOut, useSession } from "next-auth/react";
 
 const AuthSection = () => {
   const { status, data } = useSession();
+  const [sessionData, setSessionData] = useState(null);
+  const fetchCurrentSession = async () => {
+    const session = await getSession();
+    setSessionData(session);
+  };
+  useEffect(() => {
+    if (status === "authenticated") {
+      fetchCurrentSession();
+    }
+  }, [status]);
 
   return (
     <NavbarContent justify="end">
       {status === "authenticated" ? (
         <>
           <NavbarItem>
-            <Button
-              onClick={() => signOut()}
-              as={Link}
-              color="primary"
-              href="#"
-              variant="flat"
-            >
-              Sign Out
-            </Button>
+            <div>
+              {sessionData?.user?.image && (
+                <>
+                  <Popover showArrow placement="bottom">
+                    <PopoverTrigger>
+                      <Avatar
+                        className="cursor-pointer"
+                        src={sessionData?.user.image}
+                      />
+                    </PopoverTrigger>
+                    <PopoverContent className="p-1">
+                      <div className="p-4">
+                        <p className="text-gray-600 mb-5">
+                          {sessionData?.user?.email}
+                        </p>
+                        <Button
+                          onClick={() => signOut()}
+                          as={Link}
+                          color="primary"
+                          href="#"
+                          variant="flat"
+                        >
+                          Sign Out
+                        </Button>
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                </>
+              )}
+            </div>
           </NavbarItem>
         </>
       ) : (
