eace36606af86004c7495a05f1afda1e4a28f854
darshanbajania <darshanbajania1999@gmail.com>
2024-02-18 20:26:07 +0530

    feat: setup for address form validation

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -14,11 +14,46 @@ import {
   Textarea,
   useDisclosure,
 } from "@nextui-org/react";
-import React from "react";
+import React, { useState } from "react";
 
 const Checkout = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [backdrop, setBackdrop] = React.useState("opaque");
+  const [shippingAddress, setShippingAddress] = useState({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    addressLine1: "",
+    addressLine2: "",
+    city: "",
+    state: "",
+    pinCode: "",
+  });
+  const isDataValid = (fieldValue, fieldType) => {
+    switch (fieldType) {
+      case "name":
+        return /^[A-Za-z\s]+$/.test(fieldValue);
+
+      case "email":
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(fieldValue);
+
+      case "phone":
+        return /^\d+$/.test(fieldValue);
+
+      case "address":
+        return fieldValue.trim() !== "";
+
+      case "pin-code":
+        return /^\d+$/.test(fieldValue);
+
+      case "text":
+        return fieldValue.trim() !== "";
+
+      default:
+        return false;
+    }
+  };
+  console.log("🚀 ~ Checkout ~ shippingAddress:", shippingAddress);
 
   const backdrops = ["opaque", "blur", "transparent"];
 
@@ -26,12 +61,24 @@ const Checkout = () => {
     setBackdrop(backdrop);
     onOpen();
   };
+
   const citiesData = [
     {
       label: "Mumbai",
       value: "mumbai",
     },
   ];
+  const isShippingAddressValid =
+    isDataValid(shippingAddress.fullName, "name") &&
+    isDataValid(shippingAddress.email, "email") &&
+    isDataValid(shippingAddress.phoneNumber, "phone") &&
+    isDataValid(shippingAddress.addressLine1, "address") &&
+    isDataValid(shippingAddress.city, "text") &&
+    isDataValid(shippingAddress.email, "text") &&
+    isDataValid(shippingAddress.pinCode, "pin-code");
+  const onSubmit = () => {
+    handleOpen("opaque");
+  };
   return (
     <div>
       <div className="mx-auto  flex flex-wrap">
@@ -42,50 +89,129 @@ const Checkout = () => {
             <h1 className="text-lg">
               Please fill following details for checkout
             </h1>
-            <div className="p-4 rounded-lg border-1">
+            <div className="p-4 rounded-lg border-1 mt-4">
               <h3>Shipping Address</h3>
-              <Input
-                isRequired
-                type="text"
-                label="Full Name"
-                placeholder="Your Name"
-                className="my-4"
-              />
-              <Input
-                isRequired
-                type="email"
-                label="Email"
-                placeholder="junior@nextui.org"
-                className="my-4"
-              />
+              <div className="flex w-full flex-wrap md:flex-nowrap gap-4 my-4">
+                <Input
+                  isRequired
+                  type="text"
+                  label="Full Name"
+                  placeholder="Your Name"
+                  defaultValue={shippingAddress.fullName}
+                  value={shippingAddress.fullName}
+                  onValueChange={(value) => {
+                    setShippingAddress((prev) => ({
+                      ...prev,
+                      fullName: value,
+                    }));
+                  }}
+                  isInvalid={!isDataValid(shippingAddress.fullName, "name")}
+                  errorMessage={
+                    !isDataValid(shippingAddress.fullName, "name")
+                      ? "Please enter a valid name"
+                      : null
+                  }
+                />
+                <Input
+                  isRequired
+                  type="email"
+                  label="Email"
+                  placeholder="junior@nextui.org"
+                  defaultValue={shippingAddress.email}
+                  value={shippingAddress.email}
+                  onValueChange={(value) => {
+                    setShippingAddress((prev) => ({
+                      ...prev,
+                      email: value,
+                    }));
+                  }}
+                  isInvalid={!isDataValid(shippingAddress.email, "email")}
+                  errorMessage={
+                    !isDataValid(shippingAddress.email, "email")
+                      ? "Please enter a valid email"
+                      : null
+                  }
+                />
+              </div>
               <Input
                 isRequired
                 type="phone"
                 label="Phone Number"
                 placeholder="XXXXXXXXXXX"
-                className="my-4"
+                className="my-4 w-full md:w-1/2"
+                defaultValue={shippingAddress.phoneNumber}
+                value={shippingAddress.phoneNumber}
+                onValueChange={(value) => {
+                  setShippingAddress((prev) => ({
+                    ...prev,
+                    phoneNumber: value,
+                  }));
+                }}
+                isInvalid={!isDataValid(shippingAddress.phoneNumber, "phone")}
+                errorMessage={
+                  !isDataValid(shippingAddress.phoneNumber, "phone")
+                    ? "Please enter a valid phone"
+                    : null
+                }
               />
               <Input
                 isRequired
-                type="phone"
+                type="text"
                 label="Address Line 1"
                 placeholder=""
                 className="my-4"
+                defaultValue={shippingAddress.addressLine1}
+                value={shippingAddress.addressLine1}
+                onValueChange={(value) => {
+                  setShippingAddress((prev) => ({
+                    ...prev,
+                    addressLine1: value,
+                  }));
+                }}
+                isInvalid={
+                  !isDataValid(shippingAddress.addressLine1, "address")
+                }
+                errorMessage={
+                  !isDataValid(shippingAddress.addressLine1, "address")
+                    ? "Please enter a valid address"
+                    : null
+                }
               />
               <Input
-                isRequired
-                type="phone"
+                type="text"
                 label="Address Line 2"
                 placeholder=""
                 className="my-4"
+                defaultValue={shippingAddress.addressLine2}
+                value={shippingAddress.addressLine2}
+                onValueChange={(value) => {
+                  setShippingAddress((prev) => ({
+                    ...prev,
+                    addressLine2: value,
+                  }));
+                }}
               />
               <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mb-4">
                 <Input
                   isRequired
-                  type="phone"
+                  type="text"
                   label="City"
                   placeholder="Ahmedabad"
                   className="max-w-xs"
+                  defaultValue={shippingAddress.city}
+                  value={shippingAddress.city}
+                  onValueChange={(value) => {
+                    setShippingAddress((prev) => ({
+                      ...prev,
+                      city: value,
+                    }));
+                  }}
+                  isInvalid={!isDataValid(shippingAddress.city, "text")}
+                  errorMessage={
+                    !isDataValid(shippingAddress.city, "text")
+                      ? "Please enter a valid city"
+                      : null
+                  }
                 />
                 <Input
                   isRequired
@@ -93,15 +219,43 @@ const Checkout = () => {
                   label="State"
                   placeholder="Gujarat"
                   className="max-w-xs"
+                  defaultValue={shippingAddress.state}
+                  value={shippingAddress.state}
+                  onValueChange={(value) => {
+                    setShippingAddress((prev) => ({
+                      ...prev,
+                      state: value,
+                    }));
+                  }}
+                  isInvalid={!isDataValid(shippingAddress.email, "text")}
+                  errorMessage={
+                    !isDataValid(shippingAddress.email, "text")
+                      ? "Please enter a valid state"
+                      : null
+                  }
                 />
               </div>
 
               <Input
                 isRequired
-                type="phone"
+                type="text"
                 label="Pincode"
                 placeholder="xxxxxx"
                 className="max-w-xs"
+                defaultValue={shippingAddress.pinCode}
+                value={shippingAddress.pinCode}
+                onValueChange={(value) => {
+                  setShippingAddress((prev) => ({
+                    ...prev,
+                    pinCode: value,
+                  }));
+                }}
+                isInvalid={!isDataValid(shippingAddress.pinCode, "pin-code")}
+                errorMessage={
+                  !isDataValid(shippingAddress.pinCode, "pin-code")
+                    ? "Please enter a valid pinCode"
+                    : null
+                }
               />
             </div>
             <div className="p-4 rounded-lg border-1 mt-4">
@@ -134,9 +288,8 @@ const Checkout = () => {
                 <p className="text-xl">₹2500.00</p>
               </div>
               <Button
-                onClick={() => {
-                  handleOpen("opaque");
-                }}
+                isDisabled={!isShippingAddressValid}
+                onClick={onSubmit}
                 className="w-full mt-4"
                 size="lg"
               >
@@ -163,7 +316,8 @@ const Checkout = () => {
                           Order Placed Successfully. Your Order Id is 123456789
                         </p>
                         <p className="text-sm text-center">
-                          A copy of your order will be sent to your email
+                          A copy of your order will be sent to your email{" "}
+                          {isShippingAddressValid.toString()}
                         </p>
                       </ModalBody>
                       <ModalFooter>
